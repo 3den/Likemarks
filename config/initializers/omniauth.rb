fcb2adddfcc2ffe44283ccbd1bf3dedd38dbf591
Marcelo Eden <edendroid@gmail.com>
2012-07-28 01:53:24 -0300

    loggin user

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,4 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
-  #provider :facebook, "349563711788737", "8bd8263e34c961982bb4fb2b517b2594"
 end
