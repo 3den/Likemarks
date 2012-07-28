4b6c4941734e51266404a337d387ac5777d5a1d8
Marcelo Eden <edendroid@gmail.com>
2012-07-28 00:45:15 -0300

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likemarks::Application.config.secret_token = 'e52b9f08a5467af7388bcd6afc7ea6b6b5ebf839634e4cf50cd61c46bae21bc08ce58d4f2a2b6ff74715ffd48a672a4b9def756e624baa30ab62c5b7620c8bb9'
