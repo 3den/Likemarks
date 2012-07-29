1499e7372ed55840ecfa00585464aff25b446824
Marcelo Eden <edendroid@gmail.com>
2012-07-29 14:05:30 -0300

    truncate links

--- app/models/link.rb
+++ app/models/link.rb
@@ -17,10 +17,9 @@ class Link < ActiveRecord::Base
 
         link_object = Link.find_or_initialize_by_link(link)
         if link_object.new_record?
-          link_object.icon = data["icon"]
           link_object.message = data["message"]
-          link_object.name = data["name"][0...250]
-          link_object.picture = data["picture"]
+          link_object.name = data["name"][0...255]
+          link_object.picture = data["picture"][0...255]
           link_object.created_time = data["created_time"].to_time
           link_object.rank = 1
         else
