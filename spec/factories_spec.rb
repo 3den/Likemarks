2a1e710154bc4b6c0fcf0432a6c6abe3653afaa4
Marcelo Eden <edendroid@gmail.com>
2012-09-24 23:19:57 -0300

    update gems

--- spec/factories_spec.rb
+++ spec/factories_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe Factory do
+describe FactoryGirl do
   FactoryGirl.factories.each do |factory|
     context "with factory for :#{factory.name}" do
-      subject { FactoryGirl.build(factory.name) }
+      subject { build_stubbed(factory.name) }
 
       it "is valid" do
         subject.valid?.should be, subject.errors.full_messages
