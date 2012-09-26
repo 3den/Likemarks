93b7fd4fd8cc49ddf0b90a0c560e45a8475b896b
Marcelo Eden <edendroid@gmail.com>
2012-09-25 22:47:10 -0300

    hacks to fix the layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,9 @@
 class PagesController < ApplicationController
-  respond_to :json, :html
-
   def terms
   end
 
   def home
     @results = Likemarks::LinkSearch.new(self).links
-    respond_with @results
   end
 
   def scope_for(username)
